910ff28b846f72a37ee3f214edacaaf418cc226f
a1astair <a.beaumont11@gmail.com>
2016-02-20 20:10:55 -0700

    Addeded better regex for url checking and changed the code to work for all subreddits

--- Reddit Sport Stream Finder/streamfinder.py
+++ Reddit Sport Stream Finder/streamfinder.py
@@ -3,10 +3,9 @@ def main():
     
     #Check for proper parameters
     if len(sys.argv) < 3:
-        print 'Usage: python',sys.argv[0], '\'subreddit\' \'team\''
-        sys.exit()
-    elif(sys.argv[1] != "soccerstreams"):
-        print 'Error: only soccerstreams subreddit is supported'
+        print 'Usage: python',sys.argv[0], '\'subreddit\' \'a\' \'team\''
+        print 'Example1: python', sys.argv[0], 'soccerstreams Barcelona'
+        print 'Example2: python', sys.argv[0], 'NHLStreams Calgary Flames'
         sys.exit()
     if len(sys.argv) == 3:
         connect(sys.argv[1], sys.argv[2])
@@ -20,51 +19,60 @@ def main():
 def connect(sub, team):
     post_limit = 25
     p = re.compile(team, re.IGNORECASE)
-    #use regex to find the proper reddit Game Thread post
+    
     # Connect to reddit and download the subreddit front page
-    r = praw.Reddit(user_agent='Sport Streams v1.0 by /u/Qaz3r') 
+    r = praw.Reddit(user_agent='Sport Streams v1.0 by /u/a1ibs') 
     submissions = r.get_subreddit(sub).get_hot(limit=post_limit)
   
     #Process all the submissions from the specificed subreddit
     for submission in submissions:
         m = p.search(submission.title)
         if m:
-            print "Found it!", submission.title
-            print "Working on finding stream"
+            print sys.argv[0], ": Found it!", submission.title
+            print sys.argv[0], ": Working on finding stream"
             get_stream(r, submission.id)
     print "Error: No thread found with that team, please try again"
     sys.exit()        
  
 def get_stream(r, title_id):
-    url = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+
+    #John Gruber's regex to find URLs in plain text, converted to Python/Unicode
+    #See: http://daringfireball.net/2010/07/improved_regex_for_matching_urls  
+    url = re.compile(ur'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?\xab\xbb\u201c\u201d\u2018\u2019]))')
+    
     youtube = re.compile('youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([\w\-_]+)\&?')
     link = 'http://www.'
     submission = r.get_submission(submission_id=title_id)
+    
+    #Flatten comments
     comments = praw.helpers.flatten_tree(submission.comments)
+    
+    #Search through the comments until a link is found
     for comment in comments:
+    
+        #Use regex to find a match for a url or youtube
         m = re.search(url, comment.body)
         m2 = re.search(youtube, comment.body)
+        
+        #Youtube check first which will instantly load and then quit
         if m2:
-            print "Found YouTube link!", m2.group(0)
-            print "Loading link"
+            print sys.argv[0], ": Found YouTube link!", m2.group(0)
+            print sys.argv[0], ": Loading link"
             link += m2.group(0)
             webbrowser.open(link)
             sys.exit()
+            
+        #If a url is found ask the user if they would like to load it otherwise keep looking
         if m:
-            print "Found one!", m.group(0)
+            print sys.argv[0], ": Found one!", m.group(0)
             load = raw_input('Would you like to load this link?(y/n)')
             if load == 'y':
-                print "Loading link"
+                print sys.argv[0], ": Loading link"
                 webbrowser.open(m.group(0))
                 sys.exit()
             else:
-                print 'Ok still looking'
+                print sys.argv[0], ": Ok still looking"
     sys.exit()
-def parse():
-    '''soup = BeautifulSoup(http://imgur.com/a/VqUKy)
-    matches = soup.select('.album-view-image-link a')
-    matches[0]['href']
-    imgurUrlPattern = re.compile(r'(http://i.imgur.com/(.*))(\?.*)?')
-    '''
+
 if __name__ == "__main__":
     main()
